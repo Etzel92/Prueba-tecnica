16307408a81771de1515c93e59ccdbd0065859de
etz92s <etz92s@gmail.com>
2026-07-24 20:53:51 -0600

    Funcionalidad completa

--- pokeapi.js
+++ pokeapi.js
@@ -2,28 +2,33 @@ let pokemonEncontrado;
 
 //Funciones para buscar pokemon por id aleatorio o nombre
 const botonAleatorio = document.getElementById("buscarAleatorio");
-botonAleatorio.addEventListener("click", function(){
+botonAleatorio.addEventListener("click", function () {
     const numeroAleatorio = Math.floor(Math.random() * 150) + 1;
     fetch("https://pokeapi.co/api/v2/pokemon/" + numeroAleatorio)
-        .then(function (respuesta){
+        .then(function (respuesta) {
             return respuesta.json();
         })
-        .then(function(pokemon){
+        .then(function (pokemon) {
             pokemonEncontrado = pokemon;
             console.log("Información del pokemon encontrado" + pokemonEncontrado);
 
             document.getElementById("nombrePokemon").textContent = pokemon.name;
             document.getElementById("numeroPokemon").textContent = pokemon.id;
-            document.getElementById("tipoPokemon").textContent = pokemon.types[0].type.name;
+            const tipos = pokemon.types.map(function (tipo) {
+                return tipo.type.name;
+            });
+            document.getElementById("tipoPokemon").textContent = tipos.join(", ");
             document.getElementById("imagenPokemon").src = pokemon.sprites.front_default;
-        })
-        
+        });
 });
 
 const botonPorNombre = document.getElementById("buscarPorNombre");
-botonPorNombre.addEventListener("click", function(){
-    const nombre = document.getElementById("nombreBusqueda").value.trim().toLowerCase();
-        if (!nombreValido(nombre)) {
+botonPorNombre.addEventListener("click", function () {
+    const nombre = document
+        .getElementById("nombreBusqueda")
+        .value.trim()
+        .toLowerCase();
+    if (!nombreValido(nombre)) {
         return;
     }
     console.log(nombre);
@@ -34,12 +39,15 @@ botonPorNombre.addEventListener("click", function(){
             }
             return respuesta.json();
         })
-        .then(function(pokemon){
+        .then(function (pokemon) {
             pokemonEncontrado = pokemon;
             console.log("Información del pokemon encontrado" + pokemonEncontrado);
             document.getElementById("nombrePokemon").textContent = pokemon.name;
             document.getElementById("numeroPokemon").textContent = pokemon.id;
-            document.getElementById("tipoPokemon").textContent = pokemon.types[0].type.name;
+            const tipos = pokemon.types.map(function (tipo) {
+                return tipo.type.name;
+            });
+            document.getElementById("tipoPokemon").textContent = tipos.join(", ");
             document.getElementById("imagenPokemon").src = pokemon.sprites.front_default;
         })
         .catch(function (error) {
@@ -49,20 +57,20 @@ botonPorNombre.addEventListener("click", function(){
 
 //funcion para agregar pokemon al equipo
 const botonAgregarEquipo = document.getElementById("agregarEquipo");
-botonAgregarEquipo.addEventListener("click", function(){
-    if(!pokemonEncontrado){
+botonAgregarEquipo.addEventListener("click", function () {
+    if (!pokemonEncontrado) {
         alert("Primero debes buscar un Pokémon");
         return;
     }
     const tablaEquipo = document.getElementById("tablaEquipo");
 
-    if(equipoCompleto(tablaEquipo)){
+    if (equipoCompleto(tablaEquipo)) {
         return;
     }
 
     if (pokemonRepetido(tablaEquipo, pokemonEncontrado.id)) {
-    return;
-}
+        return;
+    }
 
     const fila = document.createElement("tr");
     const celdaNumero = document.createElement("td");
@@ -71,17 +79,20 @@ botonAgregarEquipo.addEventListener("click", function(){
 
     celdaNumero.textContent = pokemonEncontrado.id;
     celdaNombre.textContent = pokemonEncontrado.name;
-    celdaTipo.textContent = pokemonEncontrado.types[0].type.name;
+    const tiposPokemon = pokemonEncontrado.types.map(function (tipo) {
+        return tipo.type.name;
+    });
+    celdaTipo.textContent = tiposPokemon.join(", ");
 
     fila.appendChild(celdaNumero);
     fila.appendChild(celdaNombre);
     fila.appendChild(celdaTipo);
-    
+
     tablaEquipo.appendChild(fila);
 });
 
 // Valoramos que el maximo de pokemons sean 6
-function equipoCompleto(tablaEquipo){
+function equipoCompleto(tablaEquipo) {
     if (tablaEquipo.rows.length >= 6) {
         alert("El Equipo ya tiene 6 pokémon");
         return true;
@@ -89,10 +100,10 @@ function equipoCompleto(tablaEquipo){
     return false;
 }
 
+//Validamos que no se repita el pokémon
 function pokemonRepetido(tablaEquipo, idPokemon) {
     for (let i = 0; i < tablaEquipo.rows.length; i++) {
-        const idGuardado =
-            Number(tablaEquipo.rows[i].cells[0].textContent);
+        const idGuardado = Number(tablaEquipo.rows[i].cells[0].textContent);
 
         if (idGuardado === idPokemon) {
             alert("Este Pokémon ya está en el equipo");
@@ -109,3 +120,48 @@ function nombreValido(nombre) {
     }
     return true;
 }
+
+//Ver más información del pokémon
+const botonMasInfo = document.getElementById("verMasInformacion");
+const masInfoPokemon = document.getElementById("masInfoPokemon");
+const botonCerrarInfo = document.getElementById("botonCerrarInfo");
+
+botonMasInfo.addEventListener("click", function () {
+    if (!pokemonEncontrado) {
+        alert("Primero debes buscar un Pokémon");
+        return;
+    }
+
+    document.getElementById("alturaPokemon").textContent = pokemonEncontrado.height / 10 + " m";
+
+    document.getElementById("pesoPokemon").textContent = pokemonEncontrado.weight / 10 + " kg";
+
+    document.getElementById("experienciaPokemon").textContent = pokemonEncontrado.base_experience;
+
+    const listaHabilidades = document.getElementById("habilidadesPokemon");
+    listaHabilidades.innerHTML = "";
+    pokemonEncontrado.abilities.forEach(function (habilidad) {
+        const elementoHabilidad = document.createElement("li");
+
+        elementoHabilidad.textContent = habilidad.ability.name;
+
+        if (habilidad.is_hidden) {
+            elementoHabilidad.textContent += " (oculta)";
+        }
+
+        listaHabilidades.appendChild(elementoHabilidad);
+    });
+
+    document.getElementById("saludPokemon").textContent = pokemonEncontrado.stats[0].base_stat;
+    document.getElementById("ataquePokemon").textContent = pokemonEncontrado.stats[1].base_stat;
+    document.getElementById("defensaPokemon").textContent = pokemonEncontrado.stats[2].base_stat;
+    document.getElementById("ataqueEspecialPokemon").textContent = pokemonEncontrado.stats[3].base_stat;
+    document.getElementById("defensaEspecialPokemon").textContent = pokemonEncontrado.stats[4].base_stat;
+    document.getElementById("velocidadPokemon").textContent = pokemonEncontrado.stats[5].base_stat;
+
+    masInfoPokemon.showModal();
+});
+
+botonCerrarInfo.addEventListener("click", function () {
+    masInfoPokemon.close();
+});
