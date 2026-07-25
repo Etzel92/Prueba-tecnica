b8cd0a2b4d5d4867fde995a80d44a6ed9127116f
etz92s <etz92s@gmail.com>
2026-07-24 19:48:22 -0600

    Se agrega pokemon al equipo y se crean validaciones

--- pokeapi.js
+++ pokeapi.js
@@ -1,5 +1,6 @@
 let pokemonEncontrado;
 
+//Funciones para buscar pokemon por id aleatorio o nombre
 const botonAleatorio = document.getElementById("buscarAleatorio");
 botonAleatorio.addEventListener("click", function(){
     const numeroAleatorio = Math.floor(Math.random() * 150) + 1;
@@ -24,7 +25,10 @@ botonPorNombre.addEventListener("click", function(){
     const nombre = document.getElementById("nombreBusqueda").value;
     console.log(nombre);
     fetch("https://pokeapi.co/api/v2/pokemon/" + nombre)
-        .then(function(respuesta){
+        .then(function (respuesta) {
+            if (!respuesta.ok) {
+                throw new Error("Pokémon no encontrado");
+            }
             return respuesta.json();
         })
         .then(function(pokemon){
@@ -35,21 +39,62 @@ botonPorNombre.addEventListener("click", function(){
             document.getElementById("tipoPokemon").textContent = pokemon.types[0].type.name;
             document.getElementById("imagenPokemon").src = pokemon.sprites.front_default;
         })
+        .catch(function (error) {
+            alert(error.message);
+        });
 });
 
-const botonAgregarEquipo.addEventListener("click", function(){
-    if(!pokemonActual){
+//funcion para agregar pokemon al equipo
+const botonAgregarEquipo = document.getElementById("agregarEquipo");
+botonAgregarEquipo.addEventListener("click", function(){
+    if(!pokemonEncontrado){
         alert("Primero debes buscar un Pokémon");
         return;
     }
     const tablaEquipo = document.getElementById("tablaEquipo");
 
+    if(equipoCompleto(tablaEquipo)){
+        return;
+    }
+
+    if (pokemonRepetido(tablaEquipo, pokemonEncontrado.id)) {
+    return;
+}
+
     const fila = document.createElement("tr");
     const celdaNumero = document.createElement("td");
-    const celdaNumero = document.createElement("td");
-    const celdaNumero = document.createElement("td");
+    const celdaNombre = document.createElement("td");
+    const celdaTipo = document.createElement("td");
 
-    celdaNumero.textContent = pokemonActual.id;
-    celdaNombre.textContent = pokemonActual.nombre;
-    celdaTipo.textContent = pokemonActual.id;
+    celdaNumero.textContent = pokemonEncontrado.id;
+    celdaNombre.textContent = pokemonEncontrado.name;
+    celdaTipo.textContent = pokemonEncontrado.types[0].type.name;
+
+    fila.appendChild(celdaNumero);
+    fila.appendChild(celdaNombre);
+    fila.appendChild(celdaTipo);
+    
+    tablaEquipo.appendChild(fila);
 });
+
+// Valoramos que el maximo de pokemons sean 6
+function equipoCompleto(tablaEquipo){
+    if (tablaEquipo.rows.length >= 6) {
+        alert("El Equipo ya tiene 6 pokémon");
+        return true;
+    }
+    return false;
+}
+
+function pokemonRepetido(tablaEquipo, idPokemon) {
+    for (let i = 0; i < tablaEquipo.rows.length; i++) {
+        const idGuardado =
+            Number(tablaEquipo.rows[i].cells[0].textContent);
+
+        if (idGuardado === idPokemon) {
+            alert("Este Pokémon ya está en el equipo");
+            return true;
+        }
+    }
+    return false;
+}
